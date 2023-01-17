eb4ae45ab7d3a5ffe92331481f85705ef8a7fcc7
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-17 16:46:20 +0100

    9. Insert a document in Python tentative 2

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,4 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """insert a new documents"""
-    return mongo_collection.insert_one(kwargs).inserted_id
+    doc = mongo_collection.insert_one(kwargs)
+    return doc.inserted_id
