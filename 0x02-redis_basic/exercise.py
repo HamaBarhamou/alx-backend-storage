44b64e22831ad55e6665ac7bfd4fa2cd52869477
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-18 15:36:09 +0100

    0. Writing strings to Redis Tentatative 6

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,6 @@ class Cache:
         of the Redis client as a private variable named _redis (using
         redis.Redis()) and flush the instance using flushdb
     """
-    _redis = None
 
     def __init__(self):
         """Initiation object"""
@@ -20,5 +19,5 @@ class Cache:
     def store(self, data):
         """the store value in reduce instance"""
         key = str(uuid.uuid1())
-        self._redis.mset({key: data})
+        self._redis.set(key, data)
         return key
