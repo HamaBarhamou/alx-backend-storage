e84fc6b5151ad0d44f34e6eaae6f37cba6cc23a3
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-18 15:04:35 +0100

    0. Writing strings to Redis Tentatative 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
 import redis
 import uuid
 """Redis class"""
 
 
 class Cache:
+    """Redis class implementation"""
     _redis = None
 
     def __init__(self):
+        """Initiation object"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data):
+        """the store value in reduce instance"""
         key = str(uuid.uuid1())
         self._redis.mset({key: data})
         return key
